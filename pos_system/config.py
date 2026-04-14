36904b4b5b87b92099117001dafca82ad8cfac44
SimonBrizuela <simonbrizuela08@gmail.com>
2026-04-14 14:49:32 -0300

    Bump version to 2.0.4

--- pos_system/config.py
+++ pos_system/config.py
@@ -28,7 +28,7 @@ DATABASE_BACKUP_DIR = DATA_DIR / "backups"
 
 # Application settings
 APP_NAME = "Sistema POS"
-APP_VERSION = "2.0.1"
+APP_VERSION = "2.0.4"
 ORGANIZATION = "POS System"
 
 # UI Settings
